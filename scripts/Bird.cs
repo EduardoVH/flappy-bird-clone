e372803e012a42c2c89bb36eb94b68ef26a06b8f
Eduardo Huerta <xsplited002@gmail.com>
2024-01-19 17:52:33 -0600

    added moving barriers, audio sfx and finished game

--- scripts/Bird.cs
+++ scripts/Bird.cs
@@ -7,11 +7,17 @@ public partial class Bird : RigidBody2D
 	[Export] float speed = 200f;
 	[Export] float jumpPower = 350f;
 	[Export] float resetTimer = 1f;
+	[Export] AudioStreamWav jumpSfx;
+	[Export] AudioStreamWav scoreSfx;
+	[Export] AudioStreamWav dieSfx;
 	
+	public bool HasCrashed { get; private set; } = false;
 	
 	private Vector2 movementThisFrame = new Vector2();
 	private Vector2 jumpImpulse = new Vector2();
-	private bool hasCrashed = false;
+	private float timeSinceDeath = -0.5f;
+	private AudioStreamPlayer jumpPlayer;
+	private AudioStreamPlayer otherPlayer;
 	
 	private Bird animatedSprite2D;
 	
@@ -19,18 +25,28 @@ public partial class Bird : RigidBody2D
 	{
 		movementThisFrame.X = speed;
 		jumpImpulse.Y = -jumpPower;
-		
+
+		jumpPlayer = GetNode<AudioStreamPlayer>("JumpSounds");
+		otherPlayer = GetNode<AudioStreamPlayer>("OtherSounds");
 	}
 
 	public override void _Process(double delta)
 	{
+		if (!HasCrashed) return;
+
+		timeSinceDeath += (float)delta;
+
+		if (timeSinceDeath > resetTimer)
+		{
+			GameManager.Instance.Reload();
+		}
 	}
 	
 	
 	public override void _PhysicsProcess(double delta)
 	{
 		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("fly");
-		if (hasCrashed)
+		if (HasCrashed)
 		{
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("dead");
 			return;
@@ -43,7 +59,7 @@ public partial class Bird : RigidBody2D
 
 	public override void _Input(InputEvent @event)
 	{
-		if (hasCrashed) return;
+		if (HasCrashed) return;
 		
 		if (@event is InputEventScreenTouch { Pressed: true })
 		{
@@ -59,15 +75,39 @@ public partial class Bird : RigidBody2D
 	{
 		movementThisFrame.Y = 0f;
 		LinearVelocity = movementThisFrame;
-			
+
+		jumpPlayer.Stream = jumpSfx;
+		jumpPlayer.Play();
+		
 		ApplyImpulse(jumpImpulse);
 	}
 
 	public void StopBird()
 	{
-		hasCrashed = true;
+		if (HasCrashed) return;
+		
+		HasCrashed = true;
 		LinearVelocity = Vector2.Zero;
 		GravityScale = 0f;
 		LockRotation = false;
+		
+		otherPlayer.Stream = dieSfx;
+		otherPlayer.Play();
+	}
+
+	public void OnBodyEntered(Node body)
+	{
+		if (HasCrashed) return;
+
+		otherPlayer.Stream = dieSfx;
+		otherPlayer.Play();
+		
+		HasCrashed = true;
+	}
+
+	public void PointScoredSfx()
+	{
+		otherPlayer.Stream = scoreSfx;
+		otherPlayer.Play();
 	}
 }
--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -1,6 +1,80 @@
 using Godot;
 using System;
+using System.IO;
+using System.Linq;
 
 public partial class GameManager : Node
 {
+	
+    public static GameManager Instance;
+
+    [Export] private PackedScene barrierScene;
+    [Export] private float gapBetweenBarriers = 550f;
+    [Export] private Vector2 spawnHeightRange = new Vector2(150f, 550f);
+    [Export] private Bird bird;
+    [Export] private Node currentScene;
+    [Export] private RichTextLabel currentScoreLabel;
+    [Export] private RichTextLabel highScoreLabel;
+
+    private float lastBirdX = 0f;
+    private float birdXTally = 0f;
+    private int score = 0;
+    private int highScore = 0;
+    private string path;
+	
+    public override void _Ready()
+    {
+        Instance = this;
+
+        string data;
+        path = Path.Join(ProjectSettings.GlobalizePath("user://"), "HighScore.txt");
+
+        if (!File.Exists(path)) return;
+
+        try
+        {
+            data = File.ReadAllText(path);
+
+            int.TryParse(data, out highScore);
+            highScoreLabel.Text = highScore.ToString();
+        }
+        catch (System.Exception)
+        {
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        birdXTally += bird.Position.X - lastBirdX;
+
+        lastBirdX = bird.Position.X;
+
+        if (birdXTally >= gapBetweenBarriers)
+        {
+            birdXTally = 0f;
+            Node2D barrier = barrierScene.Instantiate<Node2D>();
+            AddChild(barrier);
+			
+            float yPos = (float)GD.RandRange(spawnHeightRange.X, spawnHeightRange.Y);
+            barrier.Position = new Vector2(bird.Position.X + gapBetweenBarriers, yPos);
+        }
+    }
+
+    public void Reload()
+    {
+        if (score > highScore)
+        {
+            File.WriteAllText(path, score.ToString());
+        }
+        
+        currentScene.GetTree().ReloadCurrentScene();
+    }
+
+    public void AddPoint()
+    {
+        score++;
+        currentScoreLabel.Text = score.ToString();
+        bird.PointScoredSfx();
+    }
+    
 }
